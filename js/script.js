303771a83d2695d38f8790beb703b4f41ab9de38
youseef ramadan <youseeframadan255@gmail.com>
2025-09-28 15:37:38 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+
+var btn1 = document.querySelector(".btnn , .btnnn , .btnnnn")
+var div = document.querySelector(".divdis")
+
+btn1.onclick = function (){
+    alert("copied")
+}
+
+var btn11 = document.querySelector(".btnnnn")
+
+btn11.onclick = function (){
+    alert("copied")
+}
+
+var btn111 = document.querySelector(".btnnn")
+
+btn111.onclick = function (){
+    alert("copied")
+}
+
+
